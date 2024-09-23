d1fe52297b7732dc11acf3bd6d4715ddc8977013
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-09-23 21:14:11 +0200

    REFACTORED Whitelab data structure

--- data_preprocessing/prepare_whitelab_data.py
+++ data_preprocessing/prepare_whitelab_data.py
@@ -4,7 +4,111 @@ import pandas as pd
 import urllib.request
 
 
-def download_and_prepare_whitelab_data(create_intermediate_files: bool = False, verbose: bool = False):
+def decoded_sequences_to_file(outfile_name: str,
+                              create_formatted_whitelab_csv: bool = False,
+                              verbose: bool = False):
+    """
+    Decodes and processes sequences from Whitelab data files, optionally creating a formatted CSV.
+
+    :param outfile_name: Base name for output files
+    :param create_formatted_whitelab_csv: Flag to create formatted Whitelab .csv file with a custom schema
+    :param verbose: Flag for verbose output
+    """
+
+    # Define special tokens used in BERT tokenization
+    special_tokens = ["[CLS]", "[SEP]", "[MASK]", "[UNK]", "[PAD]"]
+
+    tokenizer = BertTokenizer.from_pretrained('Rostlab/prot_bert_bfd')
+
+    # Load the .npz files containing the data
+    neg_data = np.load('../data/whitelab_data/hemo-negative.npz')
+    pos_data = np.load('../data/whitelab_data/hemo-positive.npz')
+
+    # Process sequences and write to files if needed
+    df_negative = write_sequences_to_list(f"../data/data_for_data_viewer/{outfile_name}_raw_negativ.txt", neg_data['arr_0'], tokenizer)
+    df_positive = write_sequences_to_list(f"../data/data_for_data_viewer/{outfile_name}_raw_positive.txt", pos_data['arr_0'], tokenizer)
+
+    if create_formatted_whitelab_csv:
+        produce_whitelab_csv_file(df_positive, df_negative, special_tokens)
+
+    format_whitelab_sequences(df_negative, f"{outfile_name}_negative", special_tokens, verbose=verbose)
+    format_whitelab_sequences(df_positive, f"{outfile_name}_positive", special_tokens, verbose=verbose)
+
+
+def write_sequences_to_list(file_name: str, sequences: np.ndarray, tokenizer: BertTokenizer) -> pd.DataFrame:
+    """
+    Decodes sequences using a tokenizer, writes to a file if required, and returns a DataFrame.
+
+    :param file_name: Name of the output file
+    :param sequences: Array of encoded sequences
+    :param tokenizer: Tokenizer for decoding the sequences
+    :param to_file: Flag to write the decoded sequences to a file
+    :return: DataFrame containing decoded sequences
+    """
+
+    decoded_sequences = [tokenizer.decode(seq) for seq in sequences]
+    special_tokens = ["[CLS]", "[SEP]", "[MASK]", "[UNK]", "[PAD]"]
+    formatted_sequences = [''.join([token for token in seq.split() if token not in special_tokens])
+                           for seq in decoded_sequences]
+    df = pd.DataFrame(formatted_sequences, columns=['sequence'])
+    df.to_csv(file_name, sep=';', index=False)
+
+    return df
+
+
+def format_whitelab_sequences(df: pd.DataFrame, outfile_name: str, special_tokens: list[str], verbose: bool = False):
+    """
+    Formats Whitelab sequences by removing special tokens and saving to a formatted CSV file.
+
+    :param df: DataFrame containing sequences
+    :param outfile_name: Output file name for formatted sequences
+    :param special_tokens: List of special tokens to be removed from sequences
+    :param verbose: Flag for verbose output
+    """
+
+    unique_sequences = {}
+
+    for sequence in df['sequence']:
+        filtered_sequence = ''.join([token for token in sequence.split() if token not in special_tokens])
+        if filtered_sequence in unique_sequences:
+            unique_sequences[filtered_sequence] += 1
+        else:
+            unique_sequences[filtered_sequence] = 1
+
+    # Convert to DataFrame and save to CSV
+    result_df = pd.DataFrame(unique_sequences.items(), columns=['sequence', 'count'])
+    result_df.to_csv(f"../data/whitelab_data/{outfile_name}_formatted.csv", sep=';', index=False)
+
+    if verbose:
+        for seq, count in unique_sequences.items():
+            print(f"Sequence: {seq} | Count: {count}")
+
+
+
+
+def produce_whitelab_csv_file(positive_data: pd.DataFrame, negative_data: pd.DataFrame, special_tokens: list[str]):
+    """
+    Formats positive and negative Whitelab data into a single CSV file.
+
+    :param positive_data: DataFrame with positive sequences
+    :param negative_data: DataFrame with negative sequences
+    :param special_tokens: List of special tokens to be removed from sequences
+    """
+
+    def format_whitelab_file(df: pd.DataFrame, label: int) -> pd.DataFrame:
+        df['sequence'] = df['sequence'].apply(
+            lambda x: ''.join([token for token in x.split() if token not in special_tokens]))
+        df['label'] = label
+        return df
+
+    negative_df = format_whitelab_file(negative_data, 0)
+    positive_df = format_whitelab_file(positive_data, 1)
+
+    result_df = pd.concat([negative_df, positive_df])
+    result_df.to_csv('../data/train_data/whitelab_hemo_data.csv', sep=';', index=False)
+
+
+def download_and_prepare_whitelab_data(create_whitelab_csv: bool = False, verbose: bool = False):
     """
     From PeptideBERT repository
     https://github.com/ChakradharG/PeptideBERT/blob/master/data/download_data.py
@@ -24,11 +128,11 @@ def download_and_prepare_whitelab_data(create_intermediate_files: bool = False, 
     def download_hemolysis():
         urllib.request.urlretrieve(
             'https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/hemo-positive.npz',
-            './hemo-positive.npz',
+            '../data/whitelab_data/hemo-positive.npz',
         )
         urllib.request.urlretrieve(
             'https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/hemo-negative.npz',
-            './hemo-negative.npz',
+            '../data/whitelab_data/hemo-negative.npz',
         )
 
     def func1(file):
@@ -46,157 +150,23 @@ def download_and_prepare_whitelab_data(create_intermediate_files: bool = False, 
         )
 
     def func2(task):
-        func1(f'./{task}-positive.npz')
-        func1(f'./{task}-negative.npz')
+        func1(f'../data/whitelab_data/{task}-positive.npz')
+        func1(f'../data/whitelab_data/{task}-negative.npz')
 
-    def main():
+    def prepare_downloaded_data():
         func2('hemo')
 
+    # First Download the Whitelab Data
     download_hemolysis()
-    main()
+
+    # Whitelab has a different encoding. PeptideBERT uses a different encoding for the sequences
+    prepare_downloaded_data()
+
     # outfile_name should stay whitelab, since this probably only works for the whitelab data
-    decoded_sequences_to_file(outfile_name='whitelab_data', create_intermediate_files=create_intermediate_files,
+    decoded_sequences_to_file(outfile_name='whitelab_data', create_formatted_whitelab_csv=create_whitelab_csv,
                               verbose=verbose)
 
 
-def decoded_sequences_to_file(outfile_name: str,
-                              create_intermediate_files: bool = False,
-                              verbose: bool = False):
-    """
-    Use for whitelab data, so we can see the sequences in the file
-    Need convert_encodings from peptideBERT to convert the sequences so the tokenizer can decode them correctly
-
-    :param outfile_name:
-    :param create_intermediate_files:
-    :param verbose:
-    :return:
-    """
-
-    special_tokens = [
-        "[CLS]",
-        "[SEP]",
-        "[MASK]",
-        "[UNK]",
-        "[PAD]"
-    ]
-
-    tokenizer = BertTokenizer.from_pretrained('Rostlab/prot_bert_bfd')
-
-    """
-    *.npz files are downloaded from here:
-    https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/hemo-positive.npz
-    https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/hemo-negative.npz
-    """
-    neg_data = np.load('./hemo-negative.npz')
-    pos_data = np.load('./hemo-positive.npz')
-
-    df_negativ = write_sequences_to_list(f"./{outfile_name}_raw_negativ.txt", neg_data['arr_0'], tokenizer,
-                                         to_file=create_intermediate_files)
-    df_positiv = write_sequences_to_list(f"./{outfile_name}_raw_positive.txt", pos_data['arr_0'], tokenizer,
-                                         to_file=create_intermediate_files)
-
-    if create_intermediate_files:
-        produce_whitelab_csv_file(df_positiv, df_negativ, special_tokens)
-
-    format_whitelab_sequences(df=df_negativ,
-                              outfile_name=f"{outfile_name}_negative",
-                              verbose=verbose,
-                              special_tokens=special_tokens)
-
-    format_whitelab_sequences(df=df_positiv,
-                              outfile_name=f"{outfile_name}_positive",
-                              verbose=verbose,
-                              special_tokens=special_tokens)
-
-
-def write_sequences_to_list(file_name: str, sequences: list, tokenizer: BertTokenizer,
-                            to_file: bool = False) -> pd.DataFrame:
-    """
-    Writes the sequences to a list and optionally to a file
-
-    :param file_name: name of the file to write to
-    :param sequences: list of sequences
-    :param tokenizer: tokenizer to decode the sequences
-    :param to_file: if the sequences should be written to a file
-
-    :return: dataframe of the sequences
-    """
-
-    sequence_list = []
-    for seq in sequences:
-        decoded_seq = tokenizer.decode(seq)
-        sequence_list.append(decoded_seq)
-
-    tmp_df = pd.DataFrame(sequence_list, columns=['sequence'])
-    if to_file:
-        tmp_df.to_csv(file_name, sep=';', index=False)
-
-    return tmp_df
-
-
-def format_whitelab_sequences(df: pd.DataFrame, outfile_name: str, special_tokens :list[str], verbose: bool = False):
-    """
-    Formats the whitelab data to a csv file with the unique sequences and their counts
-
-    :param df: dataframe with the sequences
-    :param outfile_name: name of the output file
-    """
-
-    unique_sequences = {}
-
-    for line in df.iloc[:, 0]:
-        sequence = []
-        splitted_line = line.strip().split(' ')
-        for token in splitted_line:
-            if token not in special_tokens:
-                sequence.append(token)
-        joined_sequence = ''.join(sequence)
-        if joined_sequence not in unique_sequences:
-            unique_sequences[joined_sequence] = 1
-        else:
-            unique_sequences[joined_sequence] += 1
-
-    result_df = pd.DataFrame(unique_sequences.items(), columns=['sequence', 'count'])
-    result_df.to_csv(f"./{outfile_name}_formatted.csv", sep=';', index=False)
-    if verbose:
-        for sequence, count in unique_sequences.items():
-            print(f"Seq {sequence} with count: {count}")
-
-
-def split_positive_and_negativ(file_path: str, to_file: bool = False):
-    """
-    Splits our data into positive and negative data for further analysis
-    """
-    # TODO MOVE FUNCTION TO ANOTHER FILE
-    df_to_split = pd.read_csv(file_path, sep=';')
-
-    positive_df = df_to_split[df_to_split['label'] == 1].groupby('sequence').size().reset_index(name='count')
-
-    negative_df = df_to_split[df_to_split['label'] == 0].groupby('sequence').size().reset_index(name='count')
-
-    if to_file:
-        positive_df.to_csv('our_positive.csv', sep=';', index=False)
-        negative_df.to_csv('our_negative.csv', sep=';', index=False)
-    else:
-        return positive_df, negative_df
-
-
-
-
-def produce_whitelab_csv_file(positiv_data: pd.DataFrame, negativ_data: pd.DataFrame, special_tokens: list[str]):
-    def format_whitelab_file(df: pd.DataFrame, label: int) -> pd.DataFrame:
-        df['sequence'] = df['sequence'].apply(
-            lambda x: ''.join([amino_acid for amino_acid in x.split(' ') if amino_acid not in special_tokens])
-        )
-        df['label'] = label
-        return df
-
-    negativ_df = format_whitelab_file(negativ_data, 0)
-    positiv_df = format_whitelab_file(positiv_data, 1)
-    result_df = pd.concat([negativ_df, positiv_df])
-    result_df.to_csv('./whitelab_hemo_data.csv', sep=';', index=False)
-
 if __name__ == '__main__':
-    #decoded_sequences_to_file('whitelab_data', create_intermediate_files=True, verbose=False)
-    df = pd.read_csv('splitted_hemo.csv', sep=';')
-    print(df)
+    # decoded_sequences_to_file('whitelab_data', create_intermediate_files=True, verbose=False)
+    download_and_prepare_whitelab_data(create_whitelab_csv=True, verbose=False)
