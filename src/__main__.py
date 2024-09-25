04b19d62653f382d2e7159429fd5ec5a5be3236b
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-09-25 17:11:35 +0200

    ADDED modularization and packageisation of the repo

---
+++ src/__main__.py
@@ -0,0 +1,2 @@
+# TODO DEFINE MAIN LOGIC HERE
+# TODO i think of using argparse to parse command line arguments for explicitely calling different steps (data_preprocessing, model_training, model_evaluation)
