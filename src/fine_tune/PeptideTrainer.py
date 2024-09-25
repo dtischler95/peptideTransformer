1d7465c8fc0e103f035d6e058b0dac5fee2b556d
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-09-25 20:33:19 +0200

    CHANGED log function of trainer for a prettier print of metrics

--- src/fine_tune/PeptideTrainer.py
+++ src/fine_tune/PeptideTrainer.py
@@ -7,3 +7,21 @@ class PeptideTrainer(Trainer):
             if not param.is_contiguous():
                 param.data = param.contiguous()
         super()._save(output_dir)
+
+
+    def log(self, logs):
+        # Custom logging logic
+        if self.state.epoch is not None:
+            logs["epoch"] = round(self.state.epoch, 2)
+        output = {**logs, **{"step": self.state.global_step}}
+
+        # Custom print format
+        for key, value in output.items():
+            if isinstance(value, dict):
+                for sub_key, sub_value in value.items():
+                    print(f"{sub_key.capitalize()}: {sub_value}")
+            else:
+                print(f"{key.capitalize()}: {value}")
+
+        # Call the original log method to ensure other logging mechanisms are still in place
+        super().log(logs)
