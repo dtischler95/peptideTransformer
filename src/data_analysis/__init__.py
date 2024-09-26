3b65c7d99587caff6b7853673a9b080ade603f81
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-09-26 23:28:42 +0200

    ADDED better logging and more documentation

---
+++ src/data_analysis/__init__.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.append('src/data_analysis')
+print("""
+Package for analysis the embeddings/cls token of the peptide sequences from the fine-tuned model.
+!!!STILL IN WORK AND NOT FULLY TESTED!!!
+""")
--- src/fine_tune/PeptideBERTClasses/PeptideTrainingArguments.py
+++ src/fine_tune/PeptideBERTClasses/PeptideTrainingArguments.py
@@ -1,14 +1,20 @@
+from typing import Literal
+
 from transformers import TrainingArguments
 import sys
 from pathlib import Path
 
 
 class PeptideTrainingArguments(TrainingArguments):
+    """
+    Class for adjusting TrainingArguments for PeptideBERT to our needs and collecting all necessary arguments in one place.
+    I tried to stick to commonly used values for the parameters, but feel free to adjust them to your needs.
+    """
     def __init__(self, *args,
                  lro_factor:float=0.1,
                  lro_patience:int=4,
                  lro_min_lr:float=0.000001,
-                 lro_mode:str='min',
+                 lro_mode:Literal['min', 'max']='min',
                  train_file=None,
                  model_path:str='Rostlab/prot_bert_bfd',
                  model_save_path:str='../default_path_BERT',
@@ -34,8 +40,9 @@ class PeptideTrainingArguments(TrainingArguments):
         # Create the model and plot save path if it does not exist
         Path(self.model_save_path).mkdir(parents=True, exist_ok=True)
         Path(self.plot_path).mkdir(parents=True, exist_ok=True)
-        print(Path(self.model_save_path))
         self.drop_duplicates = drop_duplicates
         self.ignore_leakage = ignore_leakage
         self.mlm_probability = mlm_probability
         self.max_length = max_length
+
+
