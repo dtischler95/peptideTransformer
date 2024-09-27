3fbc20865b590f5d1f965a548ba513c65f5e5166
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-09-27 10:32:48 +0200

    ADDED new packages for data_preprocessing

---
+++ src/data_preprocessing/binary/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# Add the project directory to the path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+print("""Package used for Preprocessing AMP data from most common AMP Data.
+\nprepare_train_file should be able to process most of those databases if custom wish is needed,\n
+ but it could be possible that you need to adjust the logic to your usecase.""")
