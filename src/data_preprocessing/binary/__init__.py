222d863e30687024852ccf937c3d955062f286bd
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-09-27 12:07:59 +0200

    FIXED typing error

--- src/data_preprocessing/binary/__init__.py
+++ src/data_preprocessing/binary/__init__.py
@@ -4,6 +4,6 @@ import sys
 # Add the project directory to the path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-print("""Package used for Preprocessing AMP data from most common AMP Data.
+print("""Package used for Preprocessing AMP data from most common AMP Databases.
 \nprepare_train_file should be able to process most of those databases if custom wish is needed,\n
  but it could be possible that you need to adjust the logic to your use-case. The other files are specific to this project""")
