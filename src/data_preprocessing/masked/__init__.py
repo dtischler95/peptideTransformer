635018847d1dbdbda6daaad7e132c4d139bc2027
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-09-27 10:32:48 +0200

    ADDED new packages for data_preprocessing

---
+++ src/data_preprocessing/masked/__init__.py
@@ -0,0 +1,6 @@
+import sys
+import os
+
+# Add the package to the path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+print("Package for collecting peptide sequences for mlm learning")
