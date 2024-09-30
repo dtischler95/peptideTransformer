13aee4053f63d08909bc808a3024839e1e773d69
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-09-30 19:16:05 +0200

    REMOVED overwritten createoptimandlr method since i actually didnt need this in the first place. lol

--- src/bert_model/PeptideBERTClasses/PeptideTrainer.py
+++ src/bert_model/PeptideBERTClasses/PeptideTrainer.py
@@ -12,7 +12,9 @@ class PeptideTrainer(Trainer):
     """
     Custom Trainer class with overridden methods.
     """
-    def __init__(self, model: Union[PreTrainedModel, nn.Module]=None, args: PeptideTrainingArguments = None, **kwargs):
+
+    def __init__(self, model: Union[PreTrainedModel, nn.Module] = None, args: PeptideTrainingArguments = None,
+                 **kwargs):
         """
         Needed so we don't get type hint Errors from the Trainer Class not recognizing the PeptideTrainingArguments class
         I see no further init logic so far
@@ -59,25 +61,6 @@ class PeptideTrainer(Trainer):
         # Call the original log method to ensure other logging mechanisms are still in place
         super().log(logs)
 
-    def create_optimizer_and_scheduler(self, num_training_steps: int):
-        """
-        Create an AdamW optimizer and a ReduceLROnPlateau scheduler for the Training Pipeline.
-        """
-
-        # TODO make this dynamically to switch to default by invoking super() method if i want to change stuff later example adam with weight decay
-        if self.optimizer is None:
-            self.optimizer = AdamW(self.model.parameters(), lr=self.args.learning_rate, weight_decay=self.args.weight_decay)
-
-        if self.lr_scheduler is None:
-            # Metric to watch for schedulers is available through training_args.
-            # If metric does not start with eval_ it will be automatically appended by the scheduler from the Trainer class
-            self.lr_scheduler = ReduceLROnPlateau(self.optimizer,
-                                                  mode=self.args.lro_mode,
-                                                  factor=self.args.lro_factor,
-                                                  patience=self.args.lro_patience,
-                                                  min_lr=self.args.lro_min_lr)
-            self._created_lr_scheduler = True
-
     def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
         """
         Perform a training step on a batch of inputs.
@@ -97,7 +80,6 @@ class PeptideTrainer(Trainer):
             `torch.Tensor`: The tensor with training loss on this batch.
         """
         loss = super().training_step(model, inputs)
-
 
         # Apply gradient norm clipping in case of exploding gradients.
         # Observed while training mlm with large train data points.
