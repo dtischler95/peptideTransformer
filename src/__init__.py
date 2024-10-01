9749603aeec59b2ea1362e136100b08f93434e86
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-10-01 15:59:52 +0200

    CHANGED small changes

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-print("""
-This is a simple CLI that can be used to fine-tune a model, preprocess data, analyze data, and reproduce our results.\n
-The Tasks performed in this Package are all about Peptide Sequences and their analysis.\n
-You should be able to fine tune a model to Peptide Sequences and add a custom Task on this fine tuned model.\n
-""")
