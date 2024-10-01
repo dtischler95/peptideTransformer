1105e245da943d8d3e672d4dad23f0811dd08b97
Daniel Tischler <daniel.tischler1995@googlemail.com>
2024-10-01 20:26:02 +0200

    IMPLEMENTED RandomWeightSampling

--- src/bert_model/transformer_metrics.py
+++ src/bert_model/transformer_metrics.py
@@ -2,35 +2,52 @@ import numpy as np
 import evaluate
 
 
-def binary_metrics(eval_preds) -> dict:
+def binary_metrics(eval_preds, debug_print: bool = True) -> dict:
     """
     Taken and adapted from:
     https://github.com/huggingface/transformers/blob/main/examples/pytorch/token-classification/run_ner.py
 
     :param eval_preds: predictions and labels
+    :param debug_print: if the predictions should be printed for debugging purposes
 
     :return: dictionary with the metrics
     """
 
     accuracy = evaluate.load("accuracy")
-    # recall = evaluate.load("recall")
-    # f1 = evaluate.load("f1")
-    # roc_auc = evaluate.load("roc_auc")
+    recall = evaluate.load("recall")
+    f1 = evaluate.load("f1")
+    roc_auc = evaluate.load("roc_auc")
 
     predictions, labels = eval_preds
     predictions = np.argmax(predictions, axis=-1)
 
     # ----- Added for debugging purposes -----
     # Save check for Prediction Bias towards one label
+    if debug_print:
+        _debug_label_printer(predictions)
 
     print(f"Acuracy Save Print: {accuracy.compute(predictions=predictions, references=labels)}")
 
     return {
-        "accuracy": accuracy.compute(predictions=predictions, references=labels)#,
-        # "recall": recall.compute(predictions=predictions, references=labels),
-        # "f1": f1.compute(predictions=predictions, references=labels),
-        # "roc_auc": roc_auc.compute(prediction_scores=predictions, references=labels)
+        "accuracy": accuracy.compute(predictions=predictions, references=labels),
+        "recall": recall.compute(predictions=predictions, references=labels),
+        "f1": f1.compute(predictions=predictions, references=labels),
+        "roc_auc": roc_auc.compute(prediction_scores=predictions, references=labels)
     }
+
+
+def _debug_label_printer(predictions):
+    label_0_counter = 0
+    label_1_counter = 0
+    for pred in predictions:
+        if pred == 0:
+            label_0_counter += 1
+        elif pred == 1:
+            label_1_counter += 1
+        else:
+            print(f"Invalid Prediction: {pred}")
+    print(f"Label 0 Counter: {label_0_counter}")
+    print(f"Label 1 Counter: {label_1_counter}")
 
 
 def mlm_metrics(eval_preds) -> dict:
