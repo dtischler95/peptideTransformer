0b8d4ac2826eb14d0df186fc2f986e8538050423
Daniel Tischler <daniel.tischler1995@googlemail.com>
2025-02-13 20:40:45 +0100

    INIT Naiv implementation of a regression head for bert. THIS IS STILL IN WORK

---
+++ src/bert_model/PeptideBERTClasses/PeptideBertForRegression.py
@@ -0,0 +1,101 @@
+from transformers import BertModel
+import torch.nn as nn
+from typing import Optional, Union, Tuple, List
+import torch
+from transformers.modeling_outputs import BaseModelOutputWithPooling
+
+
+class PeptideBertForRegression(BertModel):
+    def __init__(self, config):
+        config.return_dict = False
+        super().__init__(config)
+        # config.
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(0.15)
+        self.regression = nn.Linear(config.hidden_size, 1)
+
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def forward(
+            self,
+            input_ids: Optional[torch.Tensor] = None,
+            attention_mask: Optional[torch.Tensor] = None,
+            token_type_ids: Optional[torch.Tensor] = None,
+            position_ids: Optional[torch.Tensor] = None,
+            head_mask: Optional[torch.Tensor] = None,
+            inputs_embeds: Optional[torch.Tensor] = None,
+            encoder_hidden_states: Optional[torch.Tensor] = None,
+            encoder_attention_mask: Optional[torch.Tensor] = None,
+            past_key_values: Optional[List[torch.FloatTensor]] = None,
+            use_cache: Optional[bool] = None,
+            output_attentions: Optional[bool] = None,
+            output_hidden_states: Optional[bool] = None,
+            return_dict: Optional[bool] = None,
+            labels: Optional[torch.Tensor] = None,
+            return_pooler_output: Optional[bool] = False
+    ) -> Union[Tuple[torch.Tensor], BaseModelOutputWithPooling]:
+        """
+        Forward to propagate the input through the model adjusted to the single logit output for binary classification.
+        We also added a return_pooler_output parameter to return the pooled output for visualization purposes.
+        Also added a print_debug_graph parameter to plot the label bias in the predictions for debugging purposes.
+
+        Function overwrote the forward function of the inherited class BertForSequenceClassification.
+        We kept the Signature of the original function, so IDEs won't complain about the function signature.
+        Many of them we don't handle, but you could implement them if needed.
+
+        :param input_ids: input ids for the model
+        :param attention_mask: attention mask for the model
+        :param token_type_ids: token type ids for the model
+        :param position_ids: position ids for the model
+        :param head_mask: head mask for the model
+        :param inputs_embeds: input embeddings for the model
+        :param encoder_hidden_states: encoder hidden states for the model
+        :param encoder_attention_mask: encoder attention mask for the model
+        :param past_key_values: past key values for the model
+        :param use_cache: use cache for the model
+        :param labels: labels for the model
+        :param output_attentions: output attentions for the model
+        :param output_hidden_states: output hidden states for the model
+        :param return_dict: return dict for the model
+        :param return_pooler_output: return pooler output for the model
+        :return: loss, logits or logits
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]  # Use pooled output
+        pooled_output = self.dropout(pooled_output)
+        logits = self.regression(pooled_output)
+
+
+        # If labels are provided, compute the loss
+        loss = None
+        if labels is not None:
+            loss_fct = nn.BCELoss()  # Use Binary Cross Entropy Loss
+            loss = loss_fct(logits.view(-1), labels.view(-1).float())
+
+        if return_pooler_output:
+            return pooled_output  # Return pooled output for visualization
+
+        if not return_dict:
+            return (loss, logits) if loss is not None else logits
+
+        return BaseModelOutputWithPooling(
+            last_hidden_state=outputs.last_hidden_state,
+            pooler_output=pooled_output,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
