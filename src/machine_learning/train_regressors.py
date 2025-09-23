c102dd158f81d9878caa03669519e33b523bcfff
Daniel Tischler <daniel.tischler1995@googlemail.com>
2025-09-23 15:12:35 +0200

    CHANGED dir order

--- src/machine_learning/train_regressors.py
+++ src/machine_learning/train_regressors.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
 
     param_grids = [  # config.gb_param_grid,
         config.xtra_param_grid,
-        #config.xgb_param_grid,
+        config.xgb_param_grid,
         config.rf_param_grid,
         config.svr_param_grid
     ]
