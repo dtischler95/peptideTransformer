2b7c3473577a4fca3e960e4ff149793111476e0b
Daniel Tischler <daniel.tischler1995@googlemail.com>
2025-09-26 11:45:35 +0200

    SAVE COMMIT

--- src/bert_model/fine_tune_protBERT.py
+++ src/bert_model/fine_tune_protBERT.py
@@ -161,4 +161,4 @@ def fine_tune(config_path: str):
 
 
 if __name__ == '__main__':
-    fine_tune(config_path='peptideBERT_configs/debug_clsBERT_config.yaml')  # Path to the config file
+    fine_tune(config_path='peptideBERT_configs/debug_regBERT_config.yaml')  # Path to the config file
