3a08817ad4850ed4025ec08f334f3a7dd03a3c47
Daniel Tischler <daniel.tischler1995@googlemail.com>
2025-10-03 23:10:51 +0200

    BUGIFXES

--- src/bert_model/fine_tune_utils.py
+++ src/bert_model/fine_tune_utils.py
@@ -705,8 +705,7 @@ def get_model_stats(plot_dir: str,
     r2, mse = print_regression_metrics(y_true=target_data, y_pred=predictions, logger=logger)
 
     # plot regression train
-    make_regression_plot(y_true=target_data, y_pred=predictions, path=plot_dir + f"/{tag}_regression.pdf",
-                         tag=f"{tag}")
+    make_regression_plot(y_true=target_data, y_pred=predictions, path=plot_dir + f"/{tag}_regression.pdf")
 
     return r2, mse
 
