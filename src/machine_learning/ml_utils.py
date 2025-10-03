cf5f4b10ee643c1c09710af98481bffe96984cd8
Daniel Tischler <daniel.tischler1995@googlemail.com>
2025-10-03 19:46:35 +0200

    CHANGED new tag for regression plots

--- src/machine_learning/ml_utils.py
+++ src/machine_learning/ml_utils.py
@@ -115,8 +115,8 @@ def get_model_stats(model,
     r2, mse = print_regression_metrics(y_true=target_data, y_pred=pred_train, logger=logger)
 
     # plot regression train
-    plot_utils.plot_with_seaborn(y_true=target_data, y_pred=pred_train, path=plot_dir + f"/{tag}_regression.pdf",
-                                 tag=f"{tag}")
+    plot_utils.make_regression(y_true=target_data, y_pred=pred_train, path=plot_dir + f"/{tag}_regression.pdf",
+                               tag=f"{tag}")
 
     return r2, mse
 
