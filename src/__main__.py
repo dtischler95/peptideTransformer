1dbbf00355ff339be31069f659a5270560de5ebf
Daniel Tischler <daniel.tischler1995@googlemail.com>
2026-06-09 22:22:39 +0200

    REMOVED data_analysis CLI subcommand

--- src/__main__.py
+++ src/__main__.py
@@ -98,27 +98,6 @@ def main():
             calculate_features=not args.no_features,
         )
 
-    elif args.command == 'data_analysis':
-        ...
-        # from src.data_analysis.hemo_clustering import cluster_model_embedding
-        # from transformers import AutoModel, AutoConfig, AutoTokenizer
-        #
-        # tokenizer = AutoTokenizer.from_pretrained(args.model_path)
-        # config = AutoConfig.from_pretrained(args.model_path)
-        # model = AutoModel.from_pretrained(args.model_path, config=config)
-        #
-        #
-        #
-        # # Call your data_analysis function here
-        # if args.cluster:
-        #     #filterwarnings("ignore", category=UserWarning)
-        #     cluster_model_embedding(file_path=args.file_path,
-        #                             data_tag=args.file_path.split('/')[-1].split('.')[0],
-        #                             batch_size=8,
-        #                             plot_path=args.out_plot_path,
-        #                             tokenizer_and_model=(tokenizer, model),
-        #                             device=)  # Assuming you have a tokenizer and model to pass
-
     elif args.command == 'generate_bert_model_config':
         # Call your config generation function here
         from src.bert_model.fine_tune_utils import generate_custom_yaml_file
@@ -149,13 +128,6 @@ def parse_inputs():
     ml_regress_parser.add_argument('--data_dir', type=str, default=None, help='Path to data directory')
     ml_regress_parser.add_argument('--output_dir', type=str, default=None, help='Path to output directory')
     ml_regress_parser.add_argument('--no_features', action='store_true', help='Disable biochemical feature engineering')
-    # Subparser for data_analysis
-    data_analysis_parser = subparsers.add_parser('data_analysis', help='Analyze the data')
-    # Add arguments for data_analysis here
-    data_analysis_parser.add_argument('--cluster', action='store_true', required=True, help='Cluster with model')
-    data_analysis_parser.add_argument('--file_path', type=str, required=True, help='Path to the file')
-    data_analysis_parser.add_argument('--out_plot_path', type=str, required=True, help='Path to the Plots')
-    data_analysis_parser.add_argument('--model_path', type=str, required=False, help='Path to the Model')
     # Subparser for reproduce
     reproduce_parser = subparsers.add_parser('reproduce', help='Reproduce our results')
     # Add arguments for reproduce here
